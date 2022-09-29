61f332af5c1e2895a86274a9c830629560f6335c
Dario Ureña <darioaxel@gmail.com>
2022-09-29 09:33:46 +0200

    added supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/supabase'],
     css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
     build: {
       transpile: ["vuetify"],
