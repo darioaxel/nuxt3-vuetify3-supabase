d53a60e63eaff746660c32818d5ba0f5e09939d2
Dario Ureña <darioaxel@gmail.com>
2022-10-06 09:19:04 +0200

    added middleware and done some tests

--- components/Form/Login.vue
+++ components/Form/Login.vue
@@ -1,54 +1,48 @@
 <template>
   <v-card>
     <v-card-title>Formulario Login</v-card-title>
-    <v-form ref="form" v-model="valid" lazy-validation class="ma-2">
+    <v-form ref="form"  lazy-validation class="ma-2">
       <div class="ma-2">
       <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
       <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
       </div>
-      <v-btn color="success" class="mr-4" @click="validate">
+      <v-btn color="success" class="mr-4" @click="login">
         Login
       </v-btn>
-      <v-btn color="warning" class="mr-4" @click="reset">
+      <v-btn color="warning" class="mr-4">
         Reset Form
       </v-btn>
     </v-form>
   </v-card>
 </template>
-<script>
-export default {
-  data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    select: null,
-    items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
-    ],
-    checkbox: false,
-  }),
 
-  methods: {
-    validate() {
-      this.$refs.form.validate()
-    },
-    reset() {
-      this.$refs.form.reset()
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation()
-    },
-  },
+  <script setup lang="ts">
+
+const password = ref('');
+const name = ref('');
+const email = ref('');
+const user = useSupabaseUser();
+const { auth } = useSupabaseClient(); 
+
+const nameRules = [
+    v => !!v || 'Name is required',
+    v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+  ];
+const emailRules = [
+    v => !!v || 'E-mail is required',
+    v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+  ];
+const router = useRouter()
+// Login method using providers
+
+const login = async () => {
+  const { error } = await auth.signIn({
+          email: email.value,
+          password: password.value
+        })
+  if (error) {
+    return alert('Something went wrong !')
+  } else
+     router.push('/private-page')
 }
 </script>
